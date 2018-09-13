9dc7f4722d98dd8ea7c2f59c13751d188dc644b8
Peter Roehlen <peter.roehlen@gmail.com>
2018-09-13 17:24:48 +0700

    Create package entry point

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import Input from './Input';
+import Output from './Output';
+import PrivateKey from './PrivateKey';
+import PublicKey from './PublicKey';
+import Transaction from './Transaction';
+
+module.exports = {
+  Input,
+  Output,
+  PrivateKey,
+  PublicKey,
+  Transaction,
+}
