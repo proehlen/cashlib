3281c6b14518e9c648968549f0dd8cc8acc6261d
Peter Roehlen <peter.roehlen@gmail.com>
2018-09-21 14:20:23 +0700

    Add additional classes to library exports

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
+import Address from './Address';
 import Input from './Input';
+import Network from './Network';
+import OpCode from './OpCode';
+import opCodes from './opCodes';
+import opCodesMap from './opCodesMap';
 import Output from './Output';
 import PrivateKey from './PrivateKey';
 import PublicKey from './PublicKey';
 import Transaction from './Transaction';
 
 module.exports = {
+  Address,
   Input,
+  Network,
+  OpCode,
+  opCodes,
+  opCodesMap,
   Output,
   PrivateKey,
   PublicKey,
