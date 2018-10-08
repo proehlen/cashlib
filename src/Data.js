270de0edc2c1fec657f5e11165a347cca0dfe9fa
Peter Roehlen <peter.roehlen@gmail.com>
2018-10-08 08:00:26 +0700

    Make Data class more flexible and less error prone

--- src/Data.js
+++ src/Data.js
@@ -3,12 +3,22 @@
  */
 // @flow
 import * as stringfu from 'stringfu';
+import assert from 'assert';
+import BigInt from 'big-integer';
 
 export default class Data {
   _bytes: Uint8Array
 
-  constructor(bytes: Uint8Array) {
-    this._bytes = bytes;
+  constructor(bytes: Uint8Array | Buffer | BigInt) {
+    if (bytes instanceof Uint8Array) {
+      this._bytes = bytes;
+    } else if (bytes instanceof Buffer) {
+      this._bytes = new Uint8Array(bytes);
+    } else if (bytes instanceof BigInt) {
+      this._bytes = new Uint8Array(bytes.toArray(256).value);
+    } else {
+      throw new Error('Invalid bytes argument');
+    }
   }
 
   get bytes(): Uint8Array {
@@ -17,6 +27,14 @@ export default class Data {
 
   toHex(): string {
     return stringfu.fromBytes(this._bytes);
+  }
+
+  toArray(): Array<number> {
+    return Array.from(this._bytes);
+  }
+
+  toBigInt(): BigInt {
+    return BigInt.fromArray(this.toArray(), 256, false);
   }
 
   /**
