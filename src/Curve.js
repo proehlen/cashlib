4a3dbfbdaa0848913e24d9588671202cd3503b7a
Peter Roehlen <peter.roehlen@gmail.com>
2018-10-09 12:21:57 +0700

    Add remaining ec domain parameters to Curve, secp256k1

--- src/Curve.js
+++ src/Curve.js
@@ -1,29 +1,48 @@
 /**
  * Class representing an Elliptic Curve.
  * 
- * Note: since we presently only use secp256k1, there are two constants (a, h)
- * that aren't needed and not included in this class.
+ * Note: since we presently only use secp256k1, some simplifications are possible
+ * in elliptic curve point multiplication.  If other curves are implemented,
+ * these may not apply and class CurvePoint would have to be updated.
  */
 // @flow
 import BigInt from 'big-integer';
 
-type BasePoint = {
+type CurveBasePoint = {
   x: BigInt,
   y: BigInt,
 }
 
-export default class Curve {
-  _field: BigInt // aka 'p' - integer specifying the finite field
-  _basePoint: BasePoint // aka 'G = (xG, yG)'
-  _prime: BigInt // aka 'n' - a prime which is the order of basePoint/G
+type CurveDomainParameters = {
+  field: BigInt, // aka 'p' - integer specifying the finite field
+  basePoint: CurveBasePoint, // aka 'G = (xG, yG)'
+  prime: BigInt, // aka 'n' - a prime which is the order of basePoint/G
+  elementA: BigInt, // aka 'a' - one of two elements specifying the curve
+  elementB: BigInt, // aka 'b' - one of two elements specifying the curve
+  cofactor: BigInt, // aka 'h' - the cofactor
+}
 
-  constructor(field: BigInt, basePoint: BasePoint, prime: BigInt) {
-    this._field = field;
-    this._basePoint = basePoint;
-    this._prime = prime;
+export default class Curve {
+  _field: BigInt
+  _basePoint: CurveBasePoint
+  _prime: BigInt
+  _elementA: BigInt
+  _elementB: BigInt
+  _cofactor: BigInt
+
+  constructor(domainParameters: CurveDomainParameters) {
+    this._field = domainParameters.field;
+    this._basePoint = domainParameters.basePoint;
+    this._prime = domainParameters.prime;
+    this._elementA = domainParameters.elementA;
+    this._elementB = domainParameters.elementB;
+    this._cofactor = domainParameters.cofactor;
   }
 
   get field() { return this._field; }
   get basePoint() { return this._basePoint; }
   get prime() { return this._prime; }
+  get elementA() { return this._elementA; }
+  get elementB() { return this._elementB; }
+  get cofactor() { return this._cofactor; }
 }
--- src/secp256k1.js
+++ src/secp256k1.js
@@ -6,17 +6,17 @@ import BigInt from 'big-integer';
 
 import Curve from './Curve';
 
-// secp256k1 constants.  Unlike for other curves, we only need 3 here.  They are:
-//   field.  aka 'p' - integer specifying the finite field
-//   basePoint. aka 'G = (xG, yG)'
-//   prime. aka 'n' - a prime which is the order of basePoint/G
-const seckp256k1 = new Curve(
-  BigInt('FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEFFFFFC2F', 16),
-  {
+// Build secp256k1 curve.  
+const seckp256k1 = new Curve({
+  field: BigInt('FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEFFFFFC2F', 16),
+  basePoint: {
     x: BigInt('79BE667EF9DCBBAC55A06295CE870B07029BFCDB2DCE28D959F2815B16F81798', 16),
     y: BigInt('483ADA7726A3C4655DA4FBFC0E1108A8FD17B448A68554199C47D08FFB10D4B8', 16),
   },
-  BigInt('FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364141', 16),
-)
+  prime: BigInt('FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364141', 16),
+  elementA: BigInt('0000000000000000000000000000000000000000000000000000000000000000', 16),
+  elementB: BigInt('0000000000000000000000000000000000000000000000000000000000000007', 16),
+  cofactor: BigInt('01', 16),
+});
 
 export default seckp256k1;
